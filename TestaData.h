32c6910744f38aef843fbe2da2677667d7e848e1
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2022-11-03 19:53:24 -0300

    update

--- TestaData.h
+++ TestaData.h
@@ -26,25 +26,64 @@ TestaData::~TestaData(){
 
 // Calcula a diferença entre duas datas e retorna o valor em dias
 long long TestaData::calculaDiasEntreDatas(){
+    int dia1, mes1, ano1, dia2, mes2, ano2;
+    data1.getData(&dia1, &mes1, &ano1);
+    data2.getData(&dia2, &mes2, &ano2);
+    long long dias = 0;
+    
+    if (ano1 == ano2){
+        if (mes1 == mes2){
+            dias = dia2 - dia1;
+        }
+        else{
+            dias = (mes2 - mes1) * 30;
+            dias += dia2 - dia1;
+        }
+    }
+    else{
+        dias = (ano2 - ano1) * 365;
+        dias += (mes2 - mes1) * 30;
+        dias += dia2 - dia1;
+    }
+
+    if (dias < 0){
+        dias *= -1;
+    }
+
+    return dias;
 }
 
 // Menu de interação com o usuário
 void TestaData::menu(){
+    // declaracao de variaveis
     int *dia = new int, *mes = new int, *ano = new int;
-    string cidade;
-
+    string cidade[2];
+    // ler a primeira data
     do{
         cout << "Insira a data (dia, mes, ano):\n";
         cin >> *dia >> *mes >> *ano;
         data1.setData(dia,mes,ano);
     }while (data1.validaData() == false);
+    // ler cidade 1
     cout << "Insira a cidade: ";
     cin.ignore();
-    getline(cin,cidade);
-    cout << "O dia da semana eh: " << data1.determinaDiaSemana() << endl;
+    getline(cin,cidade[0]);
+    // ler a segunda data
+    do{
+        cout << "Insira a data (dia, mes, ano):\n";
+        cin >> *dia >> *mes >> *ano;
+        data2.setData(dia,mes,ano);
+    }while (data2.validaData() == false);
+    // ler cidade 2
+    cout << "Insira a cidade: ";
+    cin.ignore();
+    getline(cin,cidade[1]);
+    // imprimir resultados
     data1.getData(dia,mes,ano);
-    cout << "A data eh: " << *dia << '/' << *mes << '/' << *ano << endl;
-    
+    cout << "A data 1 eh: " << cidade[0] << ", " << data1.determinaDiaSemana() << " " << *dia << '/' << *mes << '/' << *ano <<  endl;
+    data2.getData(dia,mes,ano);
+    cout << "A data 2 eh: " << cidade[1] << ", " << data2.determinaDiaSemana() << " " << *dia << '/' << *mes << '/' << *ano << endl;
+    cout << calculaDiasEntreDatas() << endl;
 }
 
 #endif
